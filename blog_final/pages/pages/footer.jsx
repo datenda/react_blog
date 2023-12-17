3eb118d7700c6f5d1189beed7b1673a1297562f7
datenda <david1tenda@gmail.com>
2023-12-17 19:39:05 +0000

    Update footer

--- blog_final/pages/pages/footer.jsx
+++ blog_final/pages/pages/footer.jsx
@@ -7,52 +7,30 @@ const Footer = () => {
         <div className="flex flex-wrap items-center justify-between">
           <div className="w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
             <h2 className="text-xl font-bold mb-2">Contact Us</h2>
-            <p>Email: info@example.com</p>
-            <p>Phone: +1 (555) 123-4567</p>
-            <p>123 Main St, Cityville, USA</p>
+            <div>Email: info@example.com</div>
+            <div>Phone: +1 (555) 123-4567</div>
+            <div>123 Main St, Cityville, USA</div>
           </div>
           <div className="w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
             <h2 className="text-xl font-bold mb-2">Quick Links</h2>
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/about">About Us</a>
-              </li>
-              <li>
-                <a href="/contact">Contact</a>
-              </li>
-              <li>
-                <a href="/privacy">Privacy Policy</a>
-              </li>
-              <li>
-                <a href="/terms">Terms of Service</a>
-              </li>
-            </ul>
-          </div>
-          <div className="w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
-            <h2 className="text-xl font-bold mb-2">Connect With Us</h2>
-            <div className="flex space-x-2">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-facebook"></i>
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-linkedin"></i>
-              </a>
+            <div>
+              <div>Home</div>
+              <div>About Us</div>
+              <div>Contact</div>
+              <div>Privacy Policy</div>
+              <div>Terms of service</div>
             </div>
           </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
+            <div className="text-xl font-bold mb-2">Connect With Us</div>
+          </div>
           <div className="w-full md:w-1/2 lg:w-1/4">
-            <h2 className="text-xl font-bold mb-2">Newsletter</h2>
-            <p>Subscribe to our newsletter for updates.</p>
-            {/* Newsletter signup form or link */}
+            <div className="text-xl font-bold mb-2">Newsletter</div>
+            <div>Subscribe to our newsletter for updates.</div>
           </div>
         </div>
         <div className="mt-4 border-t border-gray-600 pt-4 text-sm text-center">
-          <p>&copy; 2023 Your Company. All rights reserved.</p>
+          <div>&copy; 2023 Your Company. All rights reserved.</div>
         </div>
       </div>
     </footer>
